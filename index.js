97b5671bc311088a5d4d86594111fd688476073d
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2025-11-06 01:05:18 +0600

    api test without client side - using postman

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://smartDbUser:CtxrWZSrabXhVtGn@cluster0.0zmmwcn.mongodb.net/?appName=Cluster0";
+
+// Create the MongoClient instance
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Smart server is running!");
 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("smart_db");
+    const productsCollection = db.collection("products");
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Smart server is running on port ${port}`);
