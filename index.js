c021b8f03f362c6afea42870d5eb5b4f68c5b614
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2025-11-09 03:34:11 +0600

    delets bids, .env added

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+console.log(process.env);
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://smartDbUser:CtxrWZSrabXhVtGn@cluster0.0zmmwcn.mongodb.net/?appName=Cluster0";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0zmmwcn.mongodb.net/?appName=Cluster0`;
 
 // Create the MongoClient instance
 const client = new MongoClient(uri, {
@@ -120,6 +122,38 @@ async function run() {
       }
       const cursor = bidsCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/bids/:productId", async (req, res) => {
+      const productId = req.params.productId;
+      const query = { product: productId };
+      const cursor = bidsCollection.find(query).sort({ bid_price: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/bids", async (req, res) => {
+      const query = {};
+      if (query.email) {
+        query.buyer_email = email;
+      }
+
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/bids", async (req, res) => {
+      const newBid = req.body;
+      const result = await bidsCollection.insertOne(newBid);
+      res.send(result);
+    });
+
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.deleteOne(query);
       res.send(result);
     });
 
